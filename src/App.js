7fde4c3931788cd28b7fd215aa575eb4f4b9bd9b
Tintswalo <110996874+calcaventia@users.noreply.github.com>
2023-07-29 05:35:13 +0200

    setting up components and pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-
-import './App.css';
-import NavBar from './components/NavBar';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import MainHero from "./components/Main";
-import AboutUs from './components/AboutUs';
-import Footer from './components/Footer';
+import AboutUs from "./components/AboutUs";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-        <div>
-          <NavBar />,
-       <MainHero />,
-       <AboutUs />,
-       <Footer />
-        </div>
-       
-
+    <div>
+      <NavBar />,
+      <MainHero />,
+      <AboutUs />,
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/RecipeTitle.js
@@ -0,0 +1,10 @@
+function RecipeTitle() {
+  return (
+    <div>
+      <h1>Cappuccino</h1>
+      <h4>Make a cappuccino like a barista!</h4>
+    </div>
+  );
+}
+
+export default RecipeTitle;
